99264dad8663694f130371591e4119b2fca69655
zeri1008 <zeri@waynehills.co>
2023-08-30 14:19:46 +0900

    playground route 처리

--- src/app/_components/header/header.component.ts
+++ src/app/_components/header/header.component.ts
@@ -24,14 +24,12 @@ export class HeaderComponent implements OnInit {
     this.router.navigate([url]);
   }
 
-  // 다른 요소 클릭했을 때 드롭다운 메뉴 닫히도록 해주어야 함.
+  // 여러 메뉴가 다 열린상태를 유지할 수 있게 할 방법에 대해서 고려해봐야함.
   onClickDropBox(type: string): void {
     if (type === 'language') {
       this.isLanguage = !this.isLanguage;
-      // this.isProfile = false; (필요 없는 작업입니다.)
     } else if (type === 'profile') {
       this.isProfile = !this.isProfile;
-      // this.isLanguage = false; 
     }
   }
 }
--- src/app/_routes/route-playground/route-playground.module.ts
+++ src/app/_routes/route-playground/route-playground.module.ts
@@ -3,11 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RoutePlaygroundComponent } from './route-playground.component';
 import { HeaderModule } from '../../_components/header/header.module';
 import { SidebarModule } from '../../_components/sidebar/sidebar.module';
-import { PlaygroundHomeModule } from './playground-home/playground-home.module';
-import { TextToImageModule } from 'src/app/_components/text-to-image/text-to-image.module';
 import { ImgRightbarModule } from 'src/app/_components/img-rightbar/img-rightbar.module';
-
-
 
 @NgModule({
   declarations: [
@@ -17,9 +13,6 @@ import { ImgRightbarModule } from 'src/app/_components/img-rightbar/img-rightbar
     CommonModule,
     HeaderModule,
     SidebarModule,
-    PlaygroundHomeModule,
-    TextToImageModule,
-    ImgRightbarModule
   ]
 })
 export class RoutePlaygroundModule { }
